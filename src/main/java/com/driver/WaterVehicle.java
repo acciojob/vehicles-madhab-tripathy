764711174fe659905ea91385d32a7bd8f8b3bd8b
Madhab Tripathy <madhabtripathy1349@gmail.com>
2023-02-11 14:34:14 +0530

    3rd Solution

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,3 +4,13 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+    public String getVehicleName(){
+        return this.name;
+    }
+    public int getVehicleCapacity(){
+        return this.capacity;
+    }
+}
