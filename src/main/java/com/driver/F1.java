4065d87beb87f94cd64666aaeb1fb89ae1e26c60
Madhab Tripathy <madhabtripathy1349@gmail.com>
2023-02-11 14:49:50 +0530

    6th Solution

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -25,18 +25,30 @@ public class F1 extends Car {
             changeGear(1);
         }
         //for all other cases, change the gear accordingly
-        else if(newSpeed >= 1 && newSpeed <= 50)
+        else if(newSpeed >= 1 && newSpeed <= 50){
             changeGear(1);
-        else if (newSpeed >= 51 && newSpeed <= 100)
+            changeSpeed(newSpeed,getCurrentDirection());
+        }
+        else if (newSpeed >= 51 && newSpeed <= 100){
             changeGear(2);
-        else if (newSpeed >= 101 && newSpeed <= 150)
+            changeSpeed(newSpeed,getCurrentDirection());
+        }
+        else if (newSpeed >= 101 && newSpeed <= 150){
             changeGear(3);
-        else if (newSpeed >= 151 && newSpeed <= 200)
+            changeSpeed(newSpeed,getCurrentDirection());
+        }
+        else if (newSpeed >= 151 && newSpeed <= 200){
             changeGear(4);
-        else if (newSpeed >= 201 && newSpeed <= 250)
+            changeSpeed(newSpeed,getCurrentDirection());
+        }
+        else if (newSpeed >= 201 && newSpeed <= 250){
             changeGear(5);
-        else if (newSpeed > 250)
+            changeSpeed(newSpeed,getCurrentDirection());
+        }
+        else if (newSpeed > 250){
             changeGear(6);
+            changeSpeed(newSpeed,getCurrentDirection());
+        }
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
